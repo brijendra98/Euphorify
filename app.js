31dabc6af105425521ab13d248e6b803e429f959
Nikshep Saravanan <Nikshep Saravanan>
2017-01-22 02:17:30 -0500

    server

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app     = express();
-
-//Note that in version 4 of express, express.bodyParser() was
-//deprecated in favor of a separate 'body-parser' module.
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-//app.use(express.bodyParser());
-
-app.post('/myaction', function(req, res) {
-  res.send('You sent the name "' + req.body.name + '".');
-});
-
-app.listen(8080, function() {
-  console.log('Server running at http://127.0.0.1:8080/');
-});
-
+1. //Import the necessary libraries/declare the necessary objects
+2. var express = require("express");
+3. var myParser = require("body-parser");
+4. var app = express();
+5. 
+6.   app.use(myParser.urlencoded({extended : true}));
+7.   app.post("/yourpath", function(request, response) {
+8.       console.log(request.body); //This prints the JSON document received (if it is a JSON document)
+9. });
+10. 
+11. //Start the server and make it listen for connections on port 8080
+12. 
+13. app.listen(8080);
