6e9177486993b72e802351117c7b5c8613149113
brijendra98 <brijendra98@gmail.com>
2017-01-27 04:06:15 -0500

    replacing with better version

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var myParser = require("body-parser");
 var request1 = require('request');
 var app = express();
 var fs = require('fs');
+app.use(myParser.json({limit: '5mb'}));
+app.use(myParser.urlencoded({limit: '5mb'}));
 
 var len;
 var phid =[];
@@ -14,16 +16,36 @@ app.post("/", function(request, response) {
     len = phid.length;
     for(i=0;i<arr.length;i++){
 
-
-            ele = ((JSON.parse(request.body["file"])["photos"]["data"][i]["images"][0]["source"]));
+        var arr1 = JSON.parse(request.body["file"])["photos"]["data"][i]["images"];
+        for(j=0;j<arr1.length;j++){
+            ele = ((JSON.parse(request.body["file"])["photos"]["data"][i]["images"][j]["source"]));
             phid.push(ele);
 
 
-            }
-        
+            request1({
+                method: 'POST',
+                url: 'https://api.projectoxford.ai/emotion/v1.0/recognize',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Ocp-Apim-Subscription-Key': '5998890bbdc74f8dbf77490639ba2604'
+                },
 
-console.log(phid);
-fs.writeFileSync("../data.txt",phid);
+                body: JSON.stringify({
+                    url: ele
+                })
+            }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var object = JSON.parse(body);
+			if(typeof object[0] !== "undefined")
+                    {emoray.push(object[0]);}
+
+                }})}
+        }
+
+var jsondata = [];
+jsondata= JSON.stringify(emoray);
+console.log((JSON.parse(JSON.stringify(JSON.parse(jsondata)[0])))["scores"]);
+fs.writeFileSync("../data.txt",jsondata);
 
 }
 
