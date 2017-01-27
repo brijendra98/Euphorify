33b70a0d17320130b664b36e1e0e7ac2479e26db
nikshepsvn <nikshepsvn@gmail.com>
2017-01-27 03:02:21 -0500

    restructuring the JSON parsing

--- app.js
+++ app.js
@@ -14,33 +14,16 @@ app.post("/", function(request, response) {
     len = phid.length;
     for(i=0;i<arr.length;i++){
 
-        var arr1 = JSON.parse(request.body["file"])["photos"]["data"][i]["images"];
-        for(j=0;j<arr1.length;j++){
-            ele = ((JSON.parse(request.body["file"])["photos"]["data"][i]["images"][j]["source"]));
+
+            ele = ((JSON.parse(request.body["file"])["photos"]["data"][i]["images"][0]["source"]));
             phid.push(ele);
 
 
-            request1({
-                method: 'POST',
-                url: 'https://api.projectoxford.ai/emotion/v1.0/recognize',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Ocp-Apim-Subscription-Key': '5998890bbdc74f8dbf77490639ba2604'
-                },
+            }
+        
 
-                body: JSON.stringify({
-                    url: ele
-                })
-            }, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var object = JSON.parse(body);
-                    emoray.push(body);
-
-                }})}
-        }
-
-console.log(emoray);
-fs.writeFileSync("../data.txt",emoray);
+console.log(phid);
+fs.writeFileSync("../data.txt",phid);
 
 }
 
